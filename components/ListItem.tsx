64696c1276a8dbe2ad8e5d0fbd293f0760ce2dd4
OKE225 <patryk.wroblewski.2006@gmail.com>
2026-07-18 10:45:34 +0200

    Add deleting location in list page

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -2,8 +2,13 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import LocationImage from "./LocationImage";
 import { router } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { supabase } from "@/lib/supabase";
+import { useAuth } from "@/AuthContext";
 
 const ListItem = ({ item }: { item: any }) => {
+  const { refetchLocations } = useAuth();
+
   const handlePress = () => {
     router.push({
       pathname: "/",
@@ -13,6 +18,24 @@ const ListItem = ({ item }: { item: any }) => {
         title: item.title,
       },
     });
+  };
+
+  const handleDelete = async () => {
+    try {
+      const { error } = await supabase
+        .from("locations")
+        .delete()
+        .eq("id", item.id);
+
+      if (error) {
+        console.error("Error deleting locations:", error);
+        return;
+      }
+
+      await refetchLocations();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -26,8 +49,18 @@ const ListItem = ({ item }: { item: any }) => {
         <LocationImage path={item.image_path} />
       </View>
 
-      <View>
-        <Text style={{ fontSize: 22, fontWeight: "500" }}>{item.title}</Text>
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}>
+          <Text style={{ fontSize: 22, fontWeight: "500" }}>{item.title}</Text>
+          <TouchableOpacity onPress={handleDelete}>
+            <MaterialIcons name="delete-outline" size={26} color="#dc2626" />
+          </TouchableOpacity>
+        </View>
         <Text style={{ color: "#3f3f46" }}>{item.description}</Text>
       </View>
     </TouchableOpacity>
